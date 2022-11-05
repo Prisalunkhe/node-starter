0ed99862c2929193144fad0e244e94a7e8421c3c
Your name here <prisalunkhe002@gmail.com>
2022-11-05 21:16:37 +0530

    Create a basic user resource

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json()
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const users = [];
+
+app.get('/users', (req, res) => {
+  return res.send(users).sendStatus(200)
 })
+app.post('/users',jsonParser, (req, res) => {
+  users.push(req.body);
+  return res.sendStatus(200)
+});
+// app.put('/users', (req, res) => {
+  
+// })
+// app.delete('/users', (req, res) => {
+  
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
