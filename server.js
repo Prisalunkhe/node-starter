dfe5eef6c4b472547aae653c8ad4a722f00351e3
Your name here <prisalunkhe002@gmail.com>
2022-11-08 18:21:35 +0530

    Created GET and POST api's for Users

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
 
-const jsonParser = bodyParser.json()
-const app = express()
-const port = 3000
+//this is how we import packages in js file
 
-const users = [];
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/users', (req, res) => {
-  return res.send(users).sendStatus(200)
-})
-app.post('/users',jsonParser, (req, res) => {
-  users.push(req.body);
-  return res.sendStatus(200)
+const port = 3000;
+const users  = [];
+//using epress import create an instance of app
+const app=express();          //express constructor
+var jsonParser = bodyParser.json();
+
+//app provide multiple methods for http verbs we can add URL and handler to it
+//first param is URL we want to accept request on,next param is call back function to handle request
+app.get('/api/users',jsonParser,(req,res)=>{
+  res.send(users);
 });
-// app.put('/users', (req, res) => {
-  
-// })
-// app.delete('/users', (req, res) => {
-  
-// })
-
+app.post('/api/users',jsonParser,(req,res)=>{
+ users.push({...req.body, id: Math.floor(Math.random() * 1000000000)})
+  res.sendStatus(200);
+});
+//listen help you to start listening to a particular port.
+//PORT is a channel available on machine
+//8081;4000;3000 are port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`User app listening on port ${port}`)
+  
 })
