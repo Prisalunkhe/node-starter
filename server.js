6d8ae7d63edcef392262d75f4ebc89b31cda3ee4
Your name here <prisalunkhe002@gmail.com>
2022-11-13 20:24:02 +0530

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 //this is how we import packages in js file
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const port = 3000;
+const databasePromise = mongoose.connect("mongodb+srv://Priyanka:PRiyanka1234@cluster0.89mga9g.mongodb.net/?retryWrites=true&w=majority");
 
 const users  = [];
+const Cat = mongoose.model('Cat', { name: String });//cat is the name of collection 
+
 //using epress import create an instance of app
-const app=express();          //express constructor
+const app=express();   
+       //express constructor
+
+app.use(express.static("static")); 
+//used to serve static file    
+
 var jsonParser = bodyParser.json();
 
 //app provide multiple methods for http verbs we can add URL and handler to it
@@ -41,11 +50,23 @@ app.post('/api/users',jsonParser,(req,res)=>{
   res.sendStatus(200);
   
 });
-
+//return promise
+app.post('/api/cat',jsonParser,async(req,res)=>{
+  const catObject = req.body;
+  const kitty = new Cat(catObject);
+  await kitty.save();
+  res.send(kitty);
+})
 //listen help you to start listening to a particular port.
 //PORT is a channel available on machine
 //8081;4000;3000 are port
-app.listen(port, () => {
-  console.log(`User app listening on port ${port}`)
-  
-})
+// app.listen(port,()=>{
+//   console.log(`User app listening on port ${port}`);
+// })
+ databasePromise
+ .then(() => 
+ app.listen(port, () => {
+  console.log(`User app listening on port ${port}`); 
+  })
+  )
+  .catch((err)=> console.log("failed to connect db",err));
---
+++ static/index.js
@@ -0,0 +1,15 @@
+document.getElementById("add-user-form").addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const name = document.querySelector("#name").value ;
+    const age = document.querySelector("#age").value ;
+
+createUser(name,age);
+})
+ async function createUser(name,age){
+    const data=await fetch('http://localhost:3000/api/users',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({name,age}),
+    });
+    console.log(data.status);
+}
